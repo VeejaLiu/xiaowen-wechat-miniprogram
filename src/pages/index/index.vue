70561bba6e1071266da8c4460496e185ef938c47
weijialiu <weijialiu@hireez.com>
2023-10-11 17:44:49 +0800

    调整样式和代码结构

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,13 @@
 <template>
     <view class="index">
         <nut-cell>
-            <nut-textarea v-model="prompt" placeholder="请输入提示词" />
+            <nut-textarea v-model="prompt" :disabled="textAreaDisabled" placeholder="请输入提示词" />
         </nut-cell>
-        <nut-button block :loading="isLoading" color="black" @click="handleClick">生成</nut-button>
-        <div v-if="imgData !== ''" class="img-box">
-            <img :src="imgData" style="width: 256px; height: 256px" />
+        <nut-button block :loading="isLoading" color="black" @click="handleClick">生成 </nut-button>
+        <div class="img-box">
+            <div v-for="(image, index) in imgData" :key="index" class="image-container">
+                <image :src="image" />
+            </div>
         </div>
     </view>
 </template>
@@ -13,36 +15,52 @@
 <script>
 import { ref } from 'vue';
 import Taro from '@tarojs/taro';
+import './index.scss';
+
 export default {
     name: 'Index',
     components: {},
-    setup() {
-        const prompt = ref('');
+    state: {},
 
+    setup() {
+        const prompt = ref('一只可爱的小猫');
+        const textAreaDisabled = ref(false);
         const isLoading = ref(false);
         const imgData = ref('');
 
-        const handleClick = () => {
+        const handleClick = async () => {
             isLoading.value = true;
-            Taro.request({
-                url: 'http://123.60.97.192/api/v1/draw',
-                method: 'POST',
-                data: {
-                    prompt: prompt.value,
-                },
-                timeout: 2 * 60 * 1000,
-                header: {
-                    'content-type': 'application/json', // 默认值
-                },
-                success: function (res) {
-                    imgData.value = 'data:image/jpeg;base64,' + res.data;
-                    isLoading.value = false;
-                },
-            });
+            textAreaDisabled.value = true;
+            try {
+                const res = await Taro.request({
+                    // url: 'http://123.60.97.192/api/v1/draw',
+                    url: 'http://localhost:10100/api/v1/draw',
+                    method: 'POST',
+                    data: {
+                        prompt: prompt.value,
+                    },
+                    timeout: 2 * 60 * 1000,
+                    header: {
+                        'content-type': 'application/json', // 默认值
+                    },
+                });
+                imgData.value = res.data.images.map((image) => image);
+            } catch (err) {
+                await Taro.showToast({
+                    title: '生成失败，请稍后再试',
+                    icon: 'error',
+                    duration: 2000,
+                });
+                console.log(err);
+            } finally {
+                isLoading.value = false;
+                textAreaDisabled.value = false;
+            }
         };
 
         return {
             prompt,
+            textAreaDisabled,
             imgData,
             isLoading,
             handleClick,
@@ -50,14 +68,3 @@ export default {
     },
 };
 </script>
-
-<style lang="scss">
-.img-box {
-    padding-top: 50px;
-    padding-bottom: 50px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-</style>
