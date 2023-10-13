2fb62916e25d2ad7d4bf3372bf697da4439a856e
Veeja Liu <veejaliu@gmail.com>
2023-10-13 05:50:41 -0500

    添加homepage页面 (#8)

--- src/pages/generate/index.vue
+++ src/pages/generate/index.vue
@@ -1,18 +1,25 @@
 <template>
     <view class="index">
-        <nut-avatar size="large" @click="goToMy">
-            <img
-                src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png"
-                alt=""
-            />
-        </nut-avatar>
+        <nut-avatar size="large" @click="goToMy"> </nut-avatar>
         <nut-button block color="" @click="goToQuota">剩余钻石: 110000 </nut-button>
+        风格：
+        <nut-cell>
+            <nut-radio-group v-model="radioVal" text-position="left" direction="horizontal">
+                <nut-radio label="1">点刺</nut-radio>
+                <nut-radio label="2">选项2</nut-radio>
+                <nut-radio label="3">选项3</nut-radio>
+            </nut-radio-group>
+        </nut-cell>
+
+        <!-- <button open-type="getPhoneNumber" bindgetphonenumber="getPhoneNumber">登录</button> -->
+
         <nut-cell>
             <nut-textarea v-model="prompt" :disabled="textAreaDisabled" placeholder="请输入提示词" />
         </nut-cell>
         <nut-button block :loading="isLoading" color="black" @click="goToGenerateResultDetail">生成 </nut-button>
+        <nut-button block :loading="isLoading" color="black" @click="getPhoneNumber">登录 </nut-button>
         <nut-grid :border="false" :column-num="2" square>
-            <nut-grid-item v-for="(image, index) in imgData" :key="index">
+            <nut-grid-item v-for="(image, index) in imgData" :key="generate">
                 <img mode="heightFix" :src="image" alt="" />
             </nut-grid-item>
         </nut-grid>
@@ -35,10 +42,28 @@ export default {
         const isLoading = ref(false);
         const imgData = ref('');
 
-        const goToMy = () => {
-            Taro.navigateTo({
-                url: '/pages/my/index',
+        const goToMy = async () => {
+            console.log('Click my, will login');
+            const res = await Taro.login();
+            console.log('Get login res', res);
+            const { code } = res;
+            console.log('Get login code', code);
+
+            const res2 = await Taro.request({
+                url: 'http://localhost:10100/api/v1/user/login',
+                method: 'POST',
+                data: {
+                    code,
+                },
+                timeout: 2 * 60 * 1000,
+                header: {
+                    'content-type': 'application/json', // 默认值
+                },
             });
+
+            // Taro.navigateTo({
+            //     url: '/pages/my/index',
+            // });
         };
 
         const goToQuota = () => {
@@ -54,7 +79,7 @@ export default {
         };
         const goToGenerateResultDetail = () => {
             Taro.navigateTo({
-                url: '/pages/generate_result_detail/index',
+                url: '/pages/generate_result_detail/index?prompt=' + prompt.value,
             });
             // isLoading.value = true;
             // textAreaDisabled.value = true;
@@ -85,6 +110,30 @@ export default {
             // }
         };
 
+        const getPhoneNumber = async () => {
+            console.log('Click getPhoneNumber');
+            const res = await Taro.getPhoneNumber();
+            console.log('Get res', res);
+            const { code } = res;
+            console.log('Get code', code);
+
+            const res2 = await Taro.request({
+                url: 'http://localhost:10100/api/v1/user/login',
+                method: 'POST',
+                data: {
+                    code,
+                },
+                timeout: 2 * 60 * 1000,
+                header: {
+                    'content-type': 'application/json', // 默认值
+                },
+            });
+
+            // Taro.navigateTo({
+            //     url: '/pages/my/index',
+            // });
+        };
+
         return {
             prompt,
             textAreaDisabled,
@@ -94,6 +143,7 @@ export default {
             goToMy,
             goToQuota,
             login,
+            getPhoneNumber,
         };
     },
 };
