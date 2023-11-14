c45b36c8b6525e8a482eaf46de00c89999f8a321
VeejaLiu <veejaliu@gmail.com>
2023-11-14 13:49:30 +0800

    删除无用的样式

--- src/pages/generate_result_detail/index.vue
+++ src/pages/generate_result_detail/index.vue
@@ -99,12 +99,3 @@ export default {
     },
 };
 </script>
-
-<style lang="scss">
-.index {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-}
-</style>
