f6252696644116dfbe03d9ba50a26df4b259b722
ZeroIce(chentingwei) <tingwei.chen@js.design>
2024-06-05 11:03:23 +0800

    feat: update setting page UI

--- src/constant/Urls.ts
+++ src/constant/Urls.ts
@@ -1,2 +1,9 @@
 export const BACKEND_URL = 'http://localhost:10101';
 export const SOURCE_PATH = "cloud://cloud1-3gjttufw3ce98d39.636c-cloud1-3gjttufw3ce98d39-1325373679";
+
+// Icon
+export const ICON_URL = {
+    BASE: {
+        right_arrow: SOURCE_PATH + "/images/base_icon/right_arrow.svg"
+    }
+}
